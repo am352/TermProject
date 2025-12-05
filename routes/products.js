91ac0c3ca62c360263c4bebc0765f95794d1e8fc
am352 <almcleroy352@gmail.com>
2025-12-04 17:14:13 -0800

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const db = require('../db'); // this is db/index.js
+const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const products = await db.getActiveProducts();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: 'Database error' });
+  }
+});
+
+module.exports = router;
