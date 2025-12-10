5494904731b2e7ab0277f3d8e7818b4e6cfeb8f0
dodgers90 <mr.trujillo.r@gmail.com>
2025-12-09 16:59:16 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('path');
+const productsRouter = require('./routes/products');
+const cartRouter = require('./routes/cart');
+
 const app = express();
 
+app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.use('/api/products', productsRouter);
+app.use('/api/cart', cartRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
