c156111fb8c41e97ea1375697300f1dfb91fd9de
dodgers90 <mr.trujillo.r@gmail.com>
2025-12-15 23:03:15 -0800

    Create profile.js

---
+++ public/js/profile.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { requireLogin } = require('../middleware/auth'); 
+const { findUserById } = require('../db'); 
+
+const router = express.Router();
+
+router.get('/profile', requireLogin, async (req, res, next) => {
+  try {
+    const user = await findUserById(req.session.userId);
+    if (!user) {
+      req.session.error = 'User not found.';
+      return res.redirect('/login');
+    }
+    res.render('profile', { title: 'Your Profile', user });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
